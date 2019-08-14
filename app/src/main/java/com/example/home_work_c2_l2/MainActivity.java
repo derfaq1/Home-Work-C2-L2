0fb18b2eebbc60afb19edaa89df4cea60dd78959
derfaq <derfaq.serg@gmail.com>
2019-08-15 00:49:06 +0300

    first-upload

---
+++ app/src/main/java/com/example/home_work_c2_l2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.home_work_c2_l2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
